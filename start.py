eff71b3ccc1b1fff9b7622deebe424557d914084
human <coder@sample.com>
2025-07-20 02:19:39 +0530

    time run

--- start.py
+++ start.py
@@ -8,8 +8,12 @@ def get_india_time():
     # Get current time in India
     india_time = datetime.now(india_tz)
     
-    # Format and display the time
-    print(f"Current time in India: {india_time.strftime('%I:%M:%S %p')}")
+    # Format the time
+    formatted_time = india_time.strftime('%I:%M:%S %p')
+    
+    # Write time to file
+    with open('time.txt', 'w') as f:
+        f.write(f"Current time in India: {formatted_time}")
 
 # Call the function
 get_india_time()
